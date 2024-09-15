5b67b49d31470635627fb1d3795cd686d4e4f8d5
Your Name <shado@LAPTOP-U7GG9IKM>
2024-09-15 14:51:05 +0800

    origin main

---
+++ strings.js
@@ -0,0 +1,18 @@
+let favActorFirstName = "Thomas Jacob";
+console.log(favActorFirstName);
+
+let favActorLastName = "Black";
+console.log(favActorLastName);
+
+let fullName = favActorFirstName + " " + favActorLastName;
+console.log(fullName);
+
+let uppercase = "thomas jacob black";
+console.log(uppercase.toUpperCase());
+
+let lowercase = "LUCAS";
+console.log(lowercase.toLowerCase());
+
+let message = "My favorite actor is THOMAS JACOB BLACK";
+message += " his best show is Silicon Valley";
+console.log(message);
